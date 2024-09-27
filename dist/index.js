64066f7ac4d82552b461dfce18a20844f6b27ca7
Shrey <shrey17122003@gmail.com>
2024-09-27 11:37:54 +0530

    change module to esnext

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,7 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const app = (0, express_1.default)();
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server Running");
